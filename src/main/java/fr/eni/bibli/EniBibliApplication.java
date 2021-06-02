31ec7f2b6820ba89033fb86828c762daba84b7ab
amuratet <amilcar.muratet@gmail.com>
2021-06-02 17:16:02 +0200

    first commit

---
+++ src/main/java/fr/eni/bibli/EniBibliApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.bibli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EniBibliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EniBibliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/bibli/EniBibliApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.bibli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EniBibliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
